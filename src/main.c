50445d5b6349c33639bc755fc26a37cec36d4a59
MLusk02 <luskiewiczmark@gmail.com>
2025-04-26 23:59:10 -0400

    makefile addition

--- src/main.c
+++ src/main.c
@@ -7,14 +7,14 @@ const char suits[4] = {'S', 'H', 'D', 'C'};
 int main(int argc, char *argv[]) {
     Card* card = (Card*) malloc(sizeof(Card));
     Card* deck = (Card*) malloc(sizeof(Card) * 52);
-    
-    for (char i = 0; i < sizeof(suits); i++) 
+
+    for (char i = 0; i < sizeof(suits); i++)
     {
         printf("Suit: %c\n", suits[i]);
-        for (int j = 2; j < 15; j++) 
+        for (int j = 2; j < 15; j++)
         {
             Card* card = (Card*) malloc(sizeof(Card));
-            if (card != NULL) 
+            if (card != NULL)
             {
                 card->suit = suits[i];
                 card->value = j;
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
         printf("\n");
     }
     /* COMMENT */
-    
+
     free(card);
     free(deck);
     return 0;
